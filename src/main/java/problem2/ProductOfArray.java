49bacee3290eb840b196a974f3f5d26f07db0171
sudipcold2 <sudipcold2@gmail.com>
2018-12-05 04:16:43 +0530

    2nd prob complete

--- src/main/java/problem2/ProductOfArray.java
+++ src/main/java/problem2/ProductOfArray.java
@@ -1,4 +1,77 @@
 package problem2;
 
 public class ProductOfArray {
+
+    /**
+     * Optimized product array approach time complexity bigoh(n) spaceComplexity bigoh(1)
+     * @param a input array
+     * @return a new modified array with product of each except i
+     */
+    public static int [] newProductArrayOptimized(int a[]){
+
+        int prodArray [] = new int[a.length];
+        int i, temp = 1;
+        for(i = 0; i < a.length; i++){
+            prodArray[i] = 1;
+        }
+
+        for(i=0;i<a.length;i++){
+            prodArray[i] = temp;
+            temp *= a[i];
+        }
+
+        temp =1;
+
+        for(i = a.length - 1; i >= 0; i--){
+            prodArray[i] *= temp;
+            temp *= a[i];
+        }
+
+        return prodArray;
+    }
+
+
+    /**
+     * BruteForce approach bigoh(n^2)
+     * @param a input array
+     * @return a new modified array with product of each except i
+     */
+    public static int [] newProductArray(int a[]){
+
+        int newArray[] = new int[a.length];
+        int product;
+        for(int i = 0;i<a.length;i++){
+            product = 1;
+            for(int j=0;j<a.length;j++){
+                if(i==j){
+                    continue;
+                }else{
+                    product *= a[j];
+                }
+            }
+            newArray[i] = product;
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3,2,1};
+        int newArray[] = newProductArray(arr);
+        for(int a : newArray){
+            System.out.print(a+" ");
+        }
+
+        System.out.println();
+
+        int newArray2[] = newProductArrayOptimized(arr);
+        for (int a : newArray2){
+            System.out.print(a + " ");
+        }
+
+
+    }
+
+
+
 }
--- src/main/java/problem3/SerializeAndDeSerializeGeneralBinaryTree.java
+++ src/main/java/problem3/SerializeAndDeSerializeGeneralBinaryTree.java
@@ -1,21 +1,100 @@
 package problem3;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
-public class SerializeGeneralBinaryTree {
 
-    String s = "";
-    public void serializeBinaryTree(Node n){
+public class SerializeAndDeSerializeGeneralBinaryTree {
+
+    private String serializedBT;
+
+    private BinaryTree binaryTree;
+
+    private final String MARKER = "-1 ";
+
+    private int val;
+
+
+
+    public SerializeAndDeSerializeGeneralBinaryTree(){
+        serializedBT = "";
+        binaryTree = new BinaryTree();
+        val = 0;
+
+    }
+
+    /**
+     * Serialize a Binary Tree
+     * @param n is root of a tree that needs to be serialized
+     */
+    private void serializeBinaryTree(Node n){
         if(n == null){
-            s = s + "-1 ";
+            serializedBT = serializedBT + MARKER;
             return ;
         }
 
-        s = s + n.val + " ";
+        serializedBT = serializedBT + n.val + " ";
         serializeBinaryTree(n.left);
         serializeBinaryTree(n.right);
     }
 
-    public String getS() {
-        return s;
+    public void setSerializedBTInFile(Node n){
+
+        serializeBinaryTree(n);
+
+        FileWriter fw= null;
+        try {
+            fw = new FileWriter("text");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // read character wise from string and write
+        // into FileWriter
+        for (int i = 0; i < serializedBT.length(); i++) {
+            try {
+                fw.write(serializedBT.charAt(i));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //close the file
+        try {
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deSerializeBinaryTree(Node root, ListIterator listIterator){
+        if (listIterator.hasNext()){
+            val = Integer.parseInt(listIterator.next().toString());
+            if(val == -1){
+                return ;
+
+            }else{
+                root = new Node(val);
+                deSerializeBinaryTree(root.left, listIterator);
+                deSerializeBinaryTree(root.right, listIterator);
+            }
+        }
+    }
+
+//    public void deSerializeBinaryTreeWrapper(ListIterator listIterator){
+//        binaryTree.root = deSerializeBinaryTree(binaryTree.root, listIterator);
+//    }
+
+    public String getSerializedBT() {
+        return serializedBT;
+    }
+
+    public BinaryTree getBinaryTree() {
+        return binaryTree;
     }
 
     public static void main(String[] args) {
@@ -27,8 +106,33 @@ public class SerializeGeneralBinaryTree {
         bt.root.left.left = new Node(4);
         bt.root.left.right = new Node(5);
 
-        SerializeGeneralBinaryTree serializeGeneralBinaryTree = new SerializeGeneralBinaryTree();
-        serializeGeneralBinaryTree.serializeBinaryTree(bt.root);
-        System.out.println(serializeGeneralBinaryTree.s);
+        SerializeAndDeSerializeGeneralBinaryTree testSerialize =
+                new SerializeAndDeSerializeGeneralBinaryTree();
+        //testSerialize.serializeBinaryTree(bt.root);
+        //System.out.println(testSerialize.serializedBT);
+        testSerialize.setSerializedBTInFile(bt.root);
+        List<String> list = new ArrayList<String>();
+        String str []= testSerialize.serializedBT.split(" ");
+        for(String s : str){
+            list.add(s);
+        }
+
+        ListIterator iterator = list.listIterator();
+
+        // Traversing the list in forward direction
+        System.out.println("Displaying list elements in forward direction : ");
+
+//        while (iterator.hasNext())
+//            System.out.print(iterator.next() + " ");
+
+ //       System.out.println();
+//
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.next());
+//        System.out.println(iterator.hasNext());
+
+        testSerialize.deSerializeBinaryTree(null, iterator);
+        System.out.println(testSerialize.binaryTree.toString());
+
     }
 }
